1bd8f5fccd22ae33d0b5f9b69487cce6dfb30a5b
Shun <him121415@gmail.com>
2021-08-16 16:28:28 +0800

    Add project files.

---
+++ Five/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Five.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Five.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(IClock clock)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
