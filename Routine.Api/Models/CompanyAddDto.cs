04c87e77914fddb41d265a06265889f97995f435
Shun <him121415@gmail.com>
2021-08-25 03:24:03 +0800

    Add: Input verification (Data Annotations)

--- Routine.Api/Models/CompanyAddDto.cs
+++ Routine.Api/Models/CompanyAddDto.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Routine.Api.Models
 {
     public class CompanyAddDto
     {
+        [Display(Name = "公司名稱")]
+        [Required(ErrorMessage = "{0}是必填的")]
+        [MaxLength(100, ErrorMessage = "{0}的長度最大為 {1}")]
         public string Name { get; set; }
+
+        [Display(Name = "公司簡介")]
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "{0}的長度範圍為 {2} ~ {1}")]
         public string Introduction { get; set; }
 
         public ICollection<EmployeeAddDto> Employees { get; set; } = new List<EmployeeAddDto>();
--- Routine.Api/Models/EmployeeAddDto.cs
+++ Routine.Api/Models/EmployeeAddDto.cs
@@ -1,14 +1,30 @@
 ﻿using Routine.Api.Entities;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Routine.Api.Models
 {
     public class EmployeeAddDto
     {
+        [Display(Name = "員工編號"), 
+        Required(ErrorMessage = "{0} 是必填的"),
+        StringLength(10, MinimumLength = 10, ErrorMessage = "{0} 的長度為 {1}")]
         public string EmployeeNo { get; set; }
+
+        [Display(Name = "名字"),
+        Required(ErrorMessage = "{0} 是必填的"),
+        MaxLength(50, ErrorMessage = "{0} 最大長度為 {1}")]
         public string FirstName { get; set; }
+
+        [Display(Name = "姓氏"),
+         Required(ErrorMessage = "{0} 是必填的"),
+         MaxLength(50, ErrorMessage = "{0} 最大長度為 {1}")]
         public string LastName { get; set; }
+
+        [Display(Name = "性別")]
         public Gender Gender { get; set; }
+
+        [Display(Name = "出生日期")]
         public DateTime DateOfBirth { get; set; }
     }
 }
